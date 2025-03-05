59a3a2d5be03ef84d97a88d6a135403d5d38cebb
Sofia Morgillo <sofia.morgillo@student.fairfield.edu>
2025-03-05 13:22:33 -0500

    added constructor, get, set methods

--- template/PhonebookEntry.java
+++ template/PhonebookEntry.java
@@ -10,7 +10,25 @@ package template;
  */
 
 class PhonebookEntry{
-
- 
+	String number;
+	String type;
+	public PhonebookEntry(String number, String type) {
+		this.number=number;
+		this.type=type;
+	}
+	
+	public String getNumber() {
+		return number;
+	}
+	public String getType() {
+		return type;
+	}
+	
+	public void setNumber(String number) {
+		this.number=number;
+	}
+	public void setType(String type) {
+		this.type=type;
+	}
 }
 	
