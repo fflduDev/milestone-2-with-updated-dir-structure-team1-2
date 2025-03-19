2696f8f3679bb41ea013a594def47f62a53282e4
Sofia Morgillo <sofia.morgillo@student.fairfield.edu>
2025-03-19 13:44:32 -0400

    overrode hashcode and equals

--- template/Contact.java
+++ template/Contact.java
@@ -1,6 +1,7 @@
 package template;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * model a Contact 
@@ -30,8 +31,30 @@ class Contact {
 	 public void addPhonebookEntry(String number, String type) {
 		 PhonebookEntry p=new PhonebookEntry(number,type);
 		 entries.add(p);
-		 System.out.println("added: "+p.getNumber());//test print, delete
+		 System.out.println("added: "+p.getPhoneNumber());//test print, delete
 	 }
 	 //TODO override 
+	 @Override
+	 public boolean equals(Object obj) {
+		 /*if (obj == this)
+			return true;
+		if (!(obj instanceof Employee))
+			return false;
+
+		Employee employee = (Employee) obj;
+		
+		//we're saying equals is true if age == age && name == name.   
+		return employee.getAge() == this.getAge() && employee.getName() == this.getName();*/
+		 if(obj==this) {return true;}
+		 if(!(obj instanceof Contact)) {return false;}
+		 Contact contact=(Contact) obj;
+		 return this.getName()==contact.getName();
+	 }
+	 public int hashCode() {
+		 //HashMap<Character, Integer> chars=new HashMap<>();//a=2 b=2 ...
+		 char first=this.getName().charAt(0);
+		 
+		 return first;
+	 }//TODO return int
 
 }
